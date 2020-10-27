d282e02ae7d67dd1b301002f829d423827808c6c
sajjad sadeghi <sadeghi.sajjad1996@outlook.com>
2020-10-27 23:52:09 +0330

    sajjad

---
+++ AirPortDataLayer/Data/AppDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AirPortModel.Models;
+
+namespace AirPortDataLayer.Data
+{
+    public class AppDatabaseContext : DbContext
+    {
+        public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<AirPort> AirPorts { get; set; }
+    }
+}
