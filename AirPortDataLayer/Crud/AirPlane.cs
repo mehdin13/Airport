87e313c9255b15c1d3f3c8f0dea2438b44dabeb1
mostafataheri242@gmail.com <mostafataheri242@gmail.com>
2020-11-17 04:05:44 +0330

    Mosi1.03

---
+++ AirPortDataLayer/Crud/AirPlane.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AirPortDataLayer.Data;
+using System.Linq;
+
+namespace AirPortDataLayer.Crud
+{
+    class AirPlane
+    {
+        private readonly AppDatabaseContext _db;
+        public AirPlane(AppDatabaseContext db)
+        {
+            _db = db;
+        }
+        public string Insert(AirPortModel.Models.AirPlane obj)
+        {
+            try
+            {
+                _db.airPlanes.Add(obj);
+                _db.SaveChanges();
+                return "Successful";
+            }
+            catch (Exception ex)
+            {
+
+                return ex.ToString();
+            }
+        }
+        public string Delete(int id)
+        {
+            try
+            {
+                var obj = _db.airPlanes.FirstOrDefault(x => x.Id == id);
+                obj.IsDelete=true;
+                _db.airPlanes.Update(obj);
+                _db.SaveChanges();
+                return "Successful";
+            }
+            catch (Exception ex)
+            {
+
+                return ex.ToString();
+            }
+        }
+        public string Update(AirPortModel.Models.AirPlane obj)
+        {
+            try
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+                return "Successful";
+            }
+            catch (Exception ex)
+            {
+
+                return ex.ToString();
+            }
+        }
+        public List<AirPortModel.Models.AirPlane> ToList()
+        {
+            return _db.airPlanes.ToList();
+        }
+        public AirPortModel.Models.AirPlane FindById(int id)
+        {
+            return _db.airPlanes.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ AirPortDataLayer/Crud/AirPort.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AirPortDataLayer.Data;
+using System.Linq;
+
+namespace AirPortDataLayer.Crud
+{
+    public class AirPort
+    {
+        private readonly AppDatabaseContext _db;
+
+        public AirPort(AppDatabaseContext db)
+        {
+            _db = db;
+        }
+        //age zood tar bidar shodi baghiasho farda mizanam 
+        // khabam gereft :(
+    }
+}
