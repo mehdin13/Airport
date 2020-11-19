323714a0ec388d80e65c4278adff1df98cacad1f
sajjad sadeghi <sadeghi.sajjad1996@outlook.com>
2020-11-19 18:35:02 +0330

    LVR 1

--- AirPortDataLayer/Crud/Gallery.cs
+++ AirPortDataLayer/Crud/Gallery.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using AirPortDataLayer.Data;
 using System.Linq;
+using AirPortDataLayer.Crud.VeiwModel;
 
 namespace AirPortDataLayer.Crud
 {
@@ -56,6 +57,18 @@ namespace AirPortDataLayer.Crud
         public List<AirPortModel.Models.Gallery> ToList()
         {
             return _db.galleries.ToList();
+        }
+        public List<ImageList> ListImage(int id)
+        {
+            ImageList image = new ImageList();
+            List<ImageList> imageLists = new List<ImageList>();
+            var gallery = _db.GalleryImages.Where(x => x.GalleryId == id);
+            foreach (var item in gallery)
+            {
+                image.Url = item.Url;
+                imageLists.Add(image);
+            }
+            return imageLists;
         }
         public AirPortModel.Models.Gallery FindById(int id)
         {
---
+++ AirPortDataLayer/Crud/VeiwModel/ImageList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirPortDataLayer.Crud.VeiwModel
+{
+    public class ImageList
+    {
+        public string Url { get; set; }
+    }
+}
