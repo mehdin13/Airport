7a9aca96ed2f7c1ad7a1dcf6a8f7a59c2da5d055
sajjad sadeghi <sadeghi.sajjad1996@outlook.com>
2020-12-02 22:46:56 +0330

    lvr 120

--- AirPort/Startup.cs
+++ AirPort/Startup.cs
@@ -1,20 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using AirPortDataLayer.Crud;
 using AirPortDataLayer.Crud.InterFace;
 using AirPortDataLayer.Data;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
@@ -44,7 +37,7 @@ namespace AirPort
             services.AddScoped<IDetail, Detail>();
             services.Configure<AppSettings>(Configuration.GetSection("TokenProvider"));
 
-            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
             services.AddControllers();
 
             #region JwtBearer
